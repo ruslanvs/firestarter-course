c4a9fee103753e090263400ad7c225845bbb7a5f
Ruslan Suvorov <ruslanvs@yahoo.com>
2020-03-21 21:51:14 -0700

    Adding Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA4IVjHEVU6fGRfXQdWUX7I130rBTFczL8",
+    authDomain: "firestarter-3ac0d.firebaseapp.com",
+    databaseURL: "https://firestarter-3ac0d.firebaseio.com",
+    projectId: "firestarter-3ac0d",
+    storageBucket: "firestarter-3ac0d.appspot.com",
+    messagingSenderId: "779568222962",
+    appId: "1:779568222962:web:55906b9fe66009ab6a366b"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA4IVjHEVU6fGRfXQdWUX7I130rBTFczL8",
+    authDomain: "firestarter-3ac0d.firebaseapp.com",
+    databaseURL: "https://firestarter-3ac0d.firebaseio.com",
+    projectId: "firestarter-3ac0d",
+    storageBucket: "firestarter-3ac0d.appspot.com",
+    messagingSenderId: "779568222962",
+    appId: "1:779568222962:web:55906b9fe66009ab6a366b"
+  },
 };
 
 /*
