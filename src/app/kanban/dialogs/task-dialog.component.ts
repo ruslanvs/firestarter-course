a47a84afa9765622f629da87147af5cf082d5efa
Ruslan Suvorov <ruslanvs@yahoo.com>
2020-03-26 22:50:03 -0700

    Delete button for boards and tasks

--- src/app/kanban/dialogs/task-dialog.component.ts
+++ src/app/kanban/dialogs/task-dialog.component.ts
@@ -31,6 +31,12 @@ import { BoardService } from '../board.service';
       <button mat-button [mat-dialog-close]="data" cdkFocusInitial>
         {{ data.isNew ? 'Add Task' : 'Update Task' }}
       </button>
+
+      <app-delete-button
+          (delete)="handleTaskDelete()"
+          *ngIf="!data.isNew"
+      >
+      </app-delete-button>
     </div>
   `
 })
@@ -39,11 +45,16 @@ export class TaskDialogComponent {
 
   constructor(
     public dialogRef: MatDialogRef<TaskDialogComponent>,
-    private boardService: BoardService,
+    private ps: BoardService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
   onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  handleTaskDelete(): void {
+    this.ps.removeTask(this.data.boardId, this.data.task);
     this.dialogRef.close();
   }
 }
